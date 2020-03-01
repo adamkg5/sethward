9465065df0efabedd69acca9261be139373b87b7
adamkg5 <35358172+adamkg5@users.noreply.github.com>
2020-03-01 02:46:02 -0600

    updates

--- shows.js
+++ shows.js
@@ -4,7 +4,7 @@ shows = {
 		"venue": "74th Annual St. Patrick's Day Celebration",
 		"city": "Shamrock, TX",
 		"ven_link": "https://www.facebook.com/shamrocktxspd/",
-		"ven_img_link": "https://www.facebook.com/601847539888714/photos/p.3447409588665814/3447409588665814/",
+		"ven_img_link": "ShamrockTX_StPat.png",
 		"start_time": "9:00 PM",
 		"with": "",
 		"note_text": "At Shamrock Country Club"
@@ -14,7 +14,7 @@ shows = {
 		"venue": "The Spot",
 		"city": "Perryton, TX",
 		"ven_link": "https://www.facebook.com/pages/category/Bar---Grill/The-spot-1929744543726571/",
-		"ven_img_link": "https://www.facebook.com/1929744543726571/photos/1931898526844506/",
+		"ven_img_link": "The_Spot.jpg",
 		"start_time": "",
 		"with": "",
 		"note_text": ""
@@ -24,7 +24,7 @@ shows = {
 		"venue": "Chilifest Music Festival",
 		"city": "Snook, TX",
 		"ven_link": "https://www.chilifest.org/",
-		"ven_img_link": "https://2qq12h8mzek4ddcxr8hr8ra7-wpengine.netdna-ssl.com/wp-content/uploads/2020/01/Chilifest-2020-event-image.jpg",
+		"ven_img_link": "Chilifest.jpg",
 		"start_time": "10:15 AM",
 		"with": "",
 		"note_text": ""
@@ -33,10 +33,10 @@ shows = {
 		"date": "05.01.2020",
 		"venue": "The Blue Light Live",
 		"city": "Lubbock, TX",
-		"ven_link": "",
-		"ven_img_link": "",
+		"ven_link": "https://www.bluelightlubbock.com/",
+		"ven_img_link": "BlueLight.jpg",
 		"start_time": "",
-		"with": "w/ Read Southall",
+		"with": "w/ Reed Brothers",
 		"note_text": ""
 	},
 	"Midland05022020": {
@@ -44,7 +44,7 @@ shows = {
 		"venue": "Lone Star Bar",
 		"city": "Midland, TX",
 		"ven_link": "https://www.facebook.com/thelonestarbar/",
-		"ven_img_link": "https://i.pinimg.com/originals/c3/06/c1/c306c17ac94247de6cc91bb5d2964943.jpg",
+		"ven_img_link": "LoneStarBar.jpg",
 		"start_time": "10:00 PM",
 		"with": "",
 		"note_text": ""
@@ -54,7 +54,7 @@ shows = {
 		"venue": "Canadian River Music Fest",
 		"city": "Canadian, TX",
 		"ven_link": "https://www.facebook.com/canadianrivermusicfestival/",
-		"ven_img_link": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/18485314_10154667362202817_1927613253422567167_n.jpg?_nc_cat=100&_nc_ohc=f8HO8jmwoMIAX8MReni&_nc_ht=scontent-dfw5-2.xx&oh=acb4efe7ccfc96a16fed3798ed28a4b5&oe=5F02D319",
+		"ven_img_link": "CanadianMusicFest.jpg",
 		"start_time": "12:00 PM",
 		"with": "",
 		"note_text": ""
@@ -64,7 +64,7 @@ shows = {
 		"venue": "Cody Crockett Memorial Ranch Rodeo Dance",
 		"city": "Pampa, TX",
 		"ven_link": "",
-		"ven_img_link": "https://features.texasmonthly.com/wp-content/uploads/sites/2/2017/07/Cody-Horse-1024x731.jpg",
+		"ven_img_link": "CodyCrockett.jpg",
 		"start_time": "10:00 PM",
 		"with": "",
 		"note_text": ""
@@ -74,7 +74,7 @@ shows = {
 		"venue": "National Cow Calling & Steak Contest",
 		"city": "Miami, TX",
 		"ven_link": "https://www.facebook.com/CowCalling2017/",
-		"ven_img_link": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/59926234_2398725263750086_5168781527697326080_n.jpg?_nc_cat=107&_nc_ohc=xakvf99ONxYAX-5Ht2G&_nc_ht=scontent-dfw5-2.xx&oh=9b65175d225035d6c6f03b7f802a24d0&oe=5EC81277",
+		"ven_img_link": "CowCalling.jpg",
 		"start_time": "7:00 PM",
 		"with": "",
 		"note_text": ""
@@ -84,7 +84,7 @@ shows = {
 		"venue": "Woodward Elks Rodeo",
 		"city": "Woodward, OK",
 		"ven_link": "https://woodwardelksrodeo.com/",
-		"ven_img_link": "https://gocountryevents.com/wp-content/uploads/job-manager-uploads/company_logo/2019/04/elks-rodeo.jpg",
+		"ven_img_link": "ElksRodeo.jpg",
 		"start_time": "10:00 PM",
 		"with": "w/ William Clark Green",
 		"note_text": ""
@@ -94,7 +94,7 @@ shows = {
 		"venue": "XIT Rodeo and Reunion",
 		"city": "Dalhart, TX",
 		"ven_link": "https://xitrodeoreunion.com/",
-		"ven_img_link": "https://xitrodeoreunion.com/wp-content/uploads/2019/06/XIT-main-social.jpg",
+		"ven_img_link": "XITRodeo.jpg",
 		"start_time": "",
 		"with": "",
 		"note_text": ""
@@ -104,7 +104,7 @@ shows = {
 		"venue": "XIT Rodeo and Reunion",
 		"city": "Dalhart, TX",
 		"ven_link": "https://xitrodeoreunion.com/",
-		"ven_img_link": "https://xitrodeoreunion.com/wp-content/uploads/2019/06/XIT-main-social.jpg",
+		"ven_img_link": "XITRodeo.jpg",
 		"start_time": "",
 		"with": "w/ TBA",
 		"note_text": ""
@@ -114,7 +114,7 @@ shows = {
 		"venue": "XIT Rodeo and Reunion",
 		"city": "Dalhart, TX",
 		"ven_link": "https://xitrodeoreunion.com/",
-		"ven_img_link": "https://xitrodeoreunion.com/wp-content/uploads/2019/06/XIT-main-social.jpg",
+		"ven_img_link": "XITRodeo.jpg",
 		"start_time": "",
 		"with": "w/ TBA",
 		"note_text": ""
